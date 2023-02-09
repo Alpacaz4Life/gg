cf49683dc9bb65dacf9c6c72254aaad5a211c724
Alpacaz4Life <124852072+Alpacaz4Life@users.noreply.github.com>
2023-02-08 21:59:09 -0500

    Add files via upload

---
+++ ConditionalStatementsExercise.java
@@ -0,0 +1,40 @@
+// Question 1
+// import java.util.Scanner;
+
+// class ConditionalStatementsExercise {
+//     public static void main(String[] args){
+//         Scanner reader = new Scanner(System.in);
+//         System.out.println("Enter a number: ");
+
+//         int number = reader.nextInt();
+
+//         if(number >= 0){
+//             System.out.println("Positive");
+//         }
+//         else{
+//             System.out.println("Negative");
+//         }
+//         reader.close();
+//     }
+// }
+
+// Question 2
+import java.util.Scanner;
+import java.lang.Math;
+
+class ConditionalStatementsExercise {
+    public static void main(String[] args){
+        Scanner reader = new Scanner(System.in);
+        
+        System.out.println("Enter the a value: ");
+        int a = reader.nextInt();
+        System.out.println("Enter the b value: ");
+        int b = reader.nextInt();
+        System.out.println("Enter the c value: ");
+        int c = reader.nextInt();
+
+        int discriminant = ()
+
+        reader.close();
+    }
+}
